d49503eb426a1a0f96fd572dd40ffe65e02e782d
01papangkorn <papang2547@gmail.com>
2024-12-11 15:07:14 +0700

    frist commit

--- src/pages/RoomDetails.js
+++ src/pages/RoomDetails.js
@@ -8,28 +8,28 @@ function RoomDetails() {
             <div className="room-header">
                 <div className="image-gallery">
                     <img
-                        src="https://via.placeholder.com/500x300"
+                        src="https://www.grandecentrepointpattaya.com/upload/rooms/mobile/deluxe-corner-seaview-800px.webp?v=66"
                         alt="Main Room"
                         className="main-image"
                     />
                     <div className="thumbnail-container">
                         <img
-                            src="https://via.placeholder.com/100x70"
+                            src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/163577354.jpg?k=9b0a6f223e654f1e1830c3809bd915537a19de843370c1964d877134ec1721b4&o=&hp=1"
                             alt="Thumbnail 1"
                             className="thumbnail-image"
                         />
                         <img
-                            src="https://via.placeholder.com/100x70"
+                            src="https://grande-centre-point-space-pattaya.hotelmix.co.th/data/Photos/OriginalPhoto/12984/1298490/1298490589/Grande-Centre-Point-Space-Pattaya-Hotel-Exterior.JPEG"
                             alt="Thumbnail 2"
                             className="thumbnail-image"
                         />
                         <img
-                            src="https://via.placeholder.com/100x70"
+                            src="https://www.grandecentrepointpattaya.com/upload/rooms/mobile/deluxe-corner-seaview-800px.webp?v=66"
                             alt="Thumbnail 3"
                             className="thumbnail-image"
                         />
                         <img
-                            src="https://via.placeholder.com/100x70"
+                            src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/163577354.jpg?k=9b0a6f223e654f1e1830c3809bd915537a19de843370c1964d877134ec1721b4&o=&hp=1"
                             alt="Thumbnail 4"
                             className="thumbnail-image"
                         />
@@ -78,19 +78,19 @@ function RoomDetails() {
                 <h4>สิ่งอำนวยความสะดวก</h4>
                 <div className="amenities-container">
                     <div className="amenity-item">
-                        <img src="https://via.placeholder.com/50" alt="Wi-Fi" />
+                        <img src="https://static.vecteezy.com/system/resources/previews/007/957/383/non_2x/wi-fi-icon-wifi-signal-icon-wireless-internet-connection-signal-illustration-isolated-on-white-background-vector.jpg" alt="Wi-Fi" className="thumbnail-image" />
                         <p>ฟรี Wi-Fi</p>
                     </div>
                     <div className="amenity-item">
-                        <img src="https://via.placeholder.com/50" alt="Breakfast" />
+                        <img src="https://cdn-icons-png.flaticon.com/512/2771/2771401.png" alt="Breakfast" className="thumbnail-image" />
                         <p>อาหารเช้าฟรี</p>
                     </div>
                     <div className="amenity-item">
-                        <img src="https://via.placeholder.com/50" alt="Parking" />
+                        <img src="https://static.vecteezy.com/system/resources/previews/003/694/243/non_2x/car-icon-in-flat-style-simple-traffic-icon-free-vector.jpg" alt="Parking" className="thumbnail-image"/>
                         <p>ที่จอดรถ</p>
                     </div>
                     <div className="amenity-item">
-                        <img src="https://via.placeholder.com/50" alt="Shuttle" />
+                        <img src="https://static.vecteezy.com/system/resources/previews/003/694/243/non_2x/car-icon-in-flat-style-simple-traffic-icon-free-vector.jpg" alt="Shuttle" className="thumbnail-image"/>
                         <p>รถรับส่ง</p>
                     </div>
                 </div>
